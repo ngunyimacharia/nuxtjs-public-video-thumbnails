143d44bedf05252c27fb476aab21b2c45c7aacc0
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2022-01-27 05:57:48 +0200

    Placeholder image added

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,11 @@
     <div class="relative bg-white">
       <div class="lg:absolute lg:inset-0">
         <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <img class="h-56 w-full object-cover lg:absolute lg:h-full" src="https://images.unsplash.com/photo-1556761175-4b46a572b786?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80" alt="">
+          <img 
+            class="h-56 w-full object-cover lg:absolute lg:h-full" 
+            src="https://via.placeholder.com/850x500?text=Public+video+thumbnail" 
+            alt="Public video thumbnail"
+          />
         </div>
       </div>
       <div class="relative py-16 px-4 sm:py-24 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:py-32 lg:grid lg:grid-cols-2">
